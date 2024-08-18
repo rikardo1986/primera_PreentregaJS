c89679a34e31801a8223d73eb36435acfd7127c7
ricardo <rikardocqa21@gmail.com>
2024-08-17 21:57:23 -0400

    tercera subida

--- JS/app.js
+++ JS/app.js
@@ -1,80 +1,81 @@
-// Saludo de bienvenida al usuario
-let username = prompt("Por favor, ingresa tu nombre de usuario:");
-let password = prompt("Por favor, ingresa tu contraseña:");
+// Logueo de admin
+let user = prompt("Por favor, ingrese el usuario:");
 
-if (username && password) {
-    alert("¡Bienvenido, " + username + "!");
-} else {
-    alert("Debes ingresar tanto el nombre de usuario como la contraseña.");
-}
+if (user === "admin") {
+    alert("Bienvenido Aministrador");
 
-// Solicitar al usuario que ingrese una opción
-function mostrarMenuPrincipal() {
-    let actividad = prompt(`Ingrese la actividad a realizar:
-    1. Agregar nuevo producto
-    2. Modificar producto
-    3. Eliminar producto
-    4. Exportar datos del inventario`);
+    // Solicitar al usuario que ingrese una opción
+    function menuPrincipal() {
+        let actividad = prompt(`Ingrese la actividad a realizar:
+        1. Agregar nuevo producto
+        2. Modificar producto
+        3. Eliminar producto
+        4. Exportar datos del inventario`);
 
-    switch (actividad) {
-        case '1':
-            agregarProducto();
-            break;
-        case '2':
-            modificarProducto();
-            break;
-        case '3':
-            eliminarProducto();
-            break;
-        case '4':
-            exportarDatos();
-            break;
-        default:
-            alert("Opción no válida");
-            mostrarMenuPrincipal();
+        switch (actividad) {
+            case '1':
+                agregarProducto();
+                break;
+            case '2':
+                modificarProducto();
+                break;
+            case '3':
+                eliminarProducto();
+                break;
+            case '4':
+                exportarDatos();
+                break;
+            default:
+                alert("Opción no válida");
+                menuPrincipal();
+        }
     }
+
+    function agregarProducto() {
+        let producto = prompt(`Elija el producto que desea agregar:
+        1. Notebook
+        2. Cámara web
+        3. Disco duro
+        4. Parlantes
+        5. Otro`);
+
+        alert("Producto agregado correctamente: " + producto);
+        menuPrincipal();
+    }
+
+    function modificarProducto() {
+        let producto = prompt(`Elija el producto que desea modificar:
+        1. Notebook
+        2. Cámara web
+        3. Disco duro
+        4. Parlantes
+        5. Otro`);
+
+        alert("Producto modificado correctamente: " + producto);
+        menuPrincipal();
+    }
+
+    function eliminarProducto() {
+        let producto = prompt(`Elija el producto que desea eliminar:
+        1. Notebook
+        2. Cámara web
+        3. Disco duro
+        4. Parlantes
+        5. Otro`);
+
+        alert("Producto eliminado correctamente: " + producto);
+        menuPrincipal();
+    }
+
+    function exportarDatos() {
+        let fecha = prompt("Elija la fecha desde cuando desea descargar un reporte (Formato: DD-MM-AAAA):");
+
+        alert("Datos exportados desde la fecha: " + fecha);
+        menuPrincipal();
+    }
+
+    menuPrincipal();
+
+} else {
+    alert("Usuario de Aministrador incorrecto");
 }
-
-function agregarProducto() {
-    let producto = prompt(`Elija el producto que desea agregar:
-    1. Notebook
-    2. Cámara web
-    3. Disco duro
-    4. Parlantes
-    5. Otro`);
-
-    alert("Producto agregado correctamente: " + producto);
-    mostrarMenuPrincipal();
-}
-
-function modificarProducto() {
-let actividad = prompt(`Ingrese la actividad a realizar:
-    1. Agregar nuevo producto
-    2. Modificar producto
-    3. Eliminar producto
-    4. Exportar datos del inventario`);
-
-    alert("Producto modificado correctamente: " + producto);
-    mostrarMenuPrincipal();
-}
-
-function eliminarProducto() {
-    let producto = prompt(`Elija el producto que desea eliminar:
-    1. Notebook
-    2. Cámara web
-    3. Disco duro
-    4. Parlantes
-    5. Otro`);
-
-    alert("Producto eliminado correctamente: " + producto);
-    mostrarMenuPrincipal();
-}
-
-function exportarDatos() {
-    let fecha = prompt("Elija la fecha desde cuando desea descargar un reporte (Formato: DD-MM-AAAA):");
-
-    alert("Datos exportados desde la fecha: " + fecha);
-    mostrarMenuPrincipal();
-}
-
-mostrarMenuPrincipal();
