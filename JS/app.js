fddf5c2c55419338c8a5dbcefee97f92bb28b553
ricardo <rikardocqa21@gmail.com>
2024-08-17 22:25:20 -0400

    subida5

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+
+let completeUser = nameUser + " " + lastNameUser;
+console.log(completeUser)
+
+let nameUser = prompt("tu nombre"); 
+let lastNameUser = prompt("tu apellido"); 
+
+console.log("Este es el nombre ", nameUser);
+console.log("Este es el apellido ",lastNameUser);
+
